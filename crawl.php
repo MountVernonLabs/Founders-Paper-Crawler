47d4effee455d3952e79ba8b08d22635eaa13696
Matt Briney <mkbriney@gmail.com>
2018-12-19 12:15:18 -0500

    Meta data parse
    Finished parsing through the meta data file, now ready to make live API calls.  May look for a storage strcuture before I do that.

--- crawl.php
+++ crawl.php
@@ -5,9 +5,17 @@ $json = file_get_contents("founders-online-metadata.json");
 $data = json_decode($json, TRUE);
 
 foreach ($data as $entry) {
-	if (in_array("Washington, George", $entry["authors"])) {	
-		echo $entry["title"]." - ";
-		echo $entry["project"];
+	if (in_array("Washington, George", $entry["authors"]) || in_array("Washington, George", $entry["recipients"])) {	
+		echo $entry["title"]."\n";
+		echo $entry["project"]."\n";
+		echo implode($entry["authors"],"; ")."\n";
+		echo implode($entry["recipients"],"; ")."\n";
+		echo $entry["date-from"]."\n";
+		echo $entry["date-to"]."\n";
+		echo $entry["permalink"]."\n";
+		echo str_replace("documents","API/docdata",$entry["permalink"])."\n"; //API URL
+		echo $entry["project"]."\n";
+		echo basename($entry["permalink"])."\n"; //ID number
 		echo "\n\n";
 	}
 }
