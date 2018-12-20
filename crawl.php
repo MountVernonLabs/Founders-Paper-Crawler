4fb1060e76892cd3d5380a38618654cb436dd52d
Matt Briney <mkbriney@gmail.com>
2018-12-19 20:51:04 -0500

    Added download tool and MySQL database import, also included rate limiting the document retrieval API.

--- crawl.php
+++ crawl.php
@@ -1,11 +1,24 @@
 <?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
 ini_set('memory_limit','1028M');
 
 $json = file_get_contents("founders-online-metadata.json");
 $data = json_decode($json, TRUE);
 
+$mysqli = new mysqli("127.0.0.1", "root", "root", "mv_dev");
+
+if ($mysqli->connect_errno) {
+	printf("Connection failed: %s\n", $mysqli->connect_error);
+	exit();
+}
+
+$count = 1;
+
 foreach ($data as $entry) {
-	if (in_array("Washington, George", $entry["authors"]) || in_array("Washington, George", $entry["recipients"])) {	
+	if (in_array("Washington, George", $entry["authors"]) || in_array("Washington, George", $entry["recipients"])) {
+		echo $count."\n";	
 		echo $entry["title"]."\n";
 		echo $entry["project"]."\n";
 		echo implode($entry["authors"],"; ")."\n";
@@ -17,11 +30,45 @@ foreach ($data as $entry) {
 		echo $entry["project"]."\n";
 		echo basename($entry["permalink"])."\n"; //ID number
 		if (in_array("Washington, George", $entry["authors"])){
+			$to_from = "from";
 			echo "Author\n";
 		}
 		if (in_array("Washington, George", $entry["recipients"])){
+			$to_from = "to";
 			echo "Recipient\n";
-		}		
+		}
+		$letter_json = file_get_contents(str_replace("documents","API/docdata",$entry["permalink"]));
+		$letter = json_decode($letter_json, TRUE);
+		echo $letter["content"]; 
+				
 		echo "\n\n";
+		$count = $count + 1;
+		
+		$mysqli->query("INSERT INTO `mv_papers`(`record`, `title`, `to_from`, `project`, `founders_url`, `date_from`, `date_to`, `authors`, `recipients`, `content`) 
+		VALUES (
+			'".basename($entry["permalink"])."', 
+			'".addslashes($entry["title"])."', 
+			'".$to_from."', 
+			'".addslashes($entry["project"])."', 
+			'".$entry["permalink"]."',
+			'".$entry["date-from"]."',
+			'".$entry["date-to"]."',
+			'".json_encode($entry["authors"])."',
+			'".json_encode($entry["recipients"])."',
+			'".addslashes($letter["content"])."'
+			
+		) 
+		ON DUPLICATE KEY UPDATE 
+			title = '".addslashes($entry["title"])."',
+			to_from = '".$to_from."', 
+			project = '".addslashes($entry["project"])."', 
+			founders_url = '".$entry["permalink"]."',
+			date_from = '".$entry["date-from"]."',
+			date_to = '".$entry["date-to"]."',
+			authors = '".json_encode($entry["authors"])."',
+			recipients = '".json_encode($entry["recipients"])."'
+		");
+		sleep(60);
+		
 	}
 }
---
+++ download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm founders-online-metadata.json
+wget https://founders.archives.gov/Metadata/founders-online-metadata.json
