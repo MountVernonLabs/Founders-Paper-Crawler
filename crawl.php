9b55a7316105237f295105cd7c2a84fd8ff01058
Matt Briney <mkbriney@gmail.com>
2018-12-21 10:44:07 -0500

    Built in resume support

--- crawl.php
+++ crawl.php
@@ -14,61 +14,71 @@ if ($mysqli->connect_errno) {
 	exit();
 }
 
-$count = 1;
+$sql = "SELECT record FROM mv_papers";
+$result = $mysqli->query($sql);
+
+$existing = [];
+foreach ($result as $record){
+	$existing[] = $record["record"];
+}
+
 
 foreach ($data as $entry) {
 	if (in_array("Washington, George", $entry["authors"]) || in_array("Washington, George", $entry["recipients"])) {
-		echo $count."\n";	
-		echo $entry["title"]."\n";
-		echo $entry["project"]."\n";
-		echo implode($entry["authors"],"; ")."\n";
-		echo implode($entry["recipients"],"; ")."\n";
-		echo $entry["date-from"]."\n";
-		echo $entry["date-to"]."\n";
-		echo $entry["permalink"]."\n";
-		echo str_replace("documents","API/docdata",$entry["permalink"])."\n"; //API URL
-		echo $entry["project"]."\n";
-		echo basename($entry["permalink"])."\n"; //ID number
-		if (in_array("Washington, George", $entry["authors"])){
-			$to_from = "from";
-			echo "Author\n";
-		}
-		if (in_array("Washington, George", $entry["recipients"])){
-			$to_from = "to";
-			echo "Recipient\n";
-		}
-		$letter_json = file_get_contents(str_replace("documents","API/docdata",$entry["permalink"]));
-		$letter = json_decode($letter_json, TRUE);
-		echo $letter["content"]; 
-				
-		echo "\n\n";
-		$count = $count + 1;
-		
-		$mysqli->query("INSERT INTO `mv_papers`(`record`, `title`, `to_from`, `project`, `founders_url`, `date_from`, `date_to`, `authors`, `recipients`, `content`) 
-		VALUES (
-			'".basename($entry["permalink"])."', 
-			'".addslashes($entry["title"])."', 
-			'".$to_from."', 
-			'".addslashes($entry["project"])."', 
-			'".$entry["permalink"]."',
-			'".$entry["date-from"]."',
-			'".$entry["date-to"]."',
-			'".json_encode($entry["authors"])."',
-			'".json_encode($entry["recipients"])."',
-			'".addslashes($letter["content"])."'
+		if (!in_array(basename($entry["permalink"]), $existing)) {
+
+			echo $entry["title"]."\n";
+			echo $entry["project"]."\n";
+			echo implode($entry["authors"],"; ")."\n";
+			echo implode($entry["recipients"],"; ")."\n";
+			echo $entry["date-from"]."\n";
+			echo $entry["date-to"]."\n";
+			echo $entry["permalink"]."\n";
+			echo str_replace("documents","API/docdata",$entry["permalink"])."\n"; //API URL
+			echo $entry["project"]."\n";
+			echo basename($entry["permalink"])."\n"; //ID number
+			if (in_array("Washington, George", $entry["authors"])){
+				$to_from = "from";
+				echo "Author\n";
+			}
+			if (in_array("Washington, George", $entry["recipients"])){
+				$to_from = "to";
+				echo "Recipient\n";
+			}
+			$letter_json = file_get_contents(str_replace("documents","API/docdata",$entry["permalink"]));
+			$letter = json_decode($letter_json, TRUE);
+			echo $letter["content"]; 
+					
+			echo "\n\n";
 			
-		) 
-		ON DUPLICATE KEY UPDATE 
-			title = '".addslashes($entry["title"])."',
-			to_from = '".$to_from."', 
-			project = '".addslashes($entry["project"])."', 
-			founders_url = '".$entry["permalink"]."',
-			date_from = '".$entry["date-from"]."',
-			date_to = '".$entry["date-to"]."',
-			authors = '".json_encode($entry["authors"])."',
-			recipients = '".json_encode($entry["recipients"])."'
-		");
-		sleep(60);
-		
+			$mysqli->query("INSERT INTO `mv_papers`(`record`, `title`, `to_from`, `project`, `founders_url`, `date_from`, `date_to`, `authors`, `recipients`, `content`) 
+			VALUES (
+				'".basename($entry["permalink"])."', 
+				'".addslashes($entry["title"])."', 
+				'".$to_from."', 
+				'".addslashes($entry["project"])."', 
+				'".$entry["permalink"]."',
+				'".$entry["date-from"]."',
+				'".$entry["date-to"]."',
+				'".json_encode($entry["authors"])."',
+				'".json_encode($entry["recipients"])."',
+				'".addslashes($letter["content"])."'
+				
+			) 
+			ON DUPLICATE KEY UPDATE 
+				title = '".addslashes($entry["title"])."',
+				to_from = '".$to_from."', 
+				project = '".addslashes($entry["project"])."', 
+				founders_url = '".$entry["permalink"]."',
+				date_from = '".$entry["date-from"]."',
+				date_to = '".$entry["date-to"]."',
+				authors = '".json_encode($entry["authors"])."',
+				recipients = '".json_encode($entry["recipients"])."'
+			");
+			sleep(300);
+		} else {
+			echo "Already recorded: ".basename($entry["permalink"])."\n";
+		}
+						
 	}
 }
